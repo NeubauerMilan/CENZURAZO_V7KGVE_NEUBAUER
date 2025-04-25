a0642e6847da8889a8498612b0c4bc943aa1a4dc
thego <thego@DESKTOP-BGAN5GR>
2025-04-25 21:41:01 +0200

    Modellek

---
+++ BACKEND/CenzurazoApi/CenzurazoApi/Models/CensorRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CenzurazoApi.Models
+{
+    public class CensorRequest
+    {
+        public string BlacklistText { get; set; } = string.Empty;
+        public string InputText { get; set; } = string.Empty;
+    }
+}
---
+++ BACKEND/CenzurazoApi/CenzurazoApi/Models/CensorResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace CenzurazoApi.Models
+{
+    public class CensorResponse
+    {
+        public string ModifiedText { get; set; } = string.Empty;
+        public List<WordFrequency> OriginalFrequencies { get; set; } = new();
+        public List<WordFrequency> ModifiedFrequencies { get; set; } = new();
+    }
+
+    public class WordFrequency
+    {
+        public string Word { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
