a49381e0d405bde26c52a4f0ed81a71b0a9e157c
thego <thego@DESKTOP-BGAN5GR>
2025-04-25 21:36:31 +0200

    Swagger

---
+++ BACKEND/CenzurazoApi/CenzurazoApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CenzurazoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API működik!");
+        }
+    }
+}
--- BACKEND/CenzurazoApi/CenzurazoApi/Program.cs
+++ BACKEND/CenzurazoApi/CenzurazoApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+
 namespace CenzurazoApi
 {
     public class Program
@@ -5,9 +7,24 @@ namespace CenzurazoApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.MapControllers();
 
             app.Run();
         }
